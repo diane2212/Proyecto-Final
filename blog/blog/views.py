31539c9048db3f033cdbe5f19f571655e01ca5ce
diane2212 <Lu44zarate@gmail.com>
2024-10-11 00:10:05 -0300

    Delete blog directory

--- blog/blog/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-#desde las vistas generales que tiene django...
-#vistas basadas en clases, o en funciones
-class IndexView(TemplateView):
-    template_name = "index.html"
